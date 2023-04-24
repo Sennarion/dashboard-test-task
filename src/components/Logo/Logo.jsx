a8a1d0b0d3d386185b234bf67275ddb57cbaaa69
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-04-24 19:51:06 +0300

    final

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import { StyledLogo, LogoIcon, LogoText, LogoVersion } from './Logo.styled';
 
 export default function Logo() {
   return (
-    <StyledLogo>
+    <StyledLogo href="/">
       <LogoIcon width="37" height="37">
         <use href={`${icons}#icon-setting`}></use>
       </LogoIcon>
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -19,32 +19,30 @@ export default function Table({ customers }) {
   ];
 
   return (
-    <>
-      <StyledTable>
-        <TableHead>
-          <TableHeadRow>
-            {fields.map(field => (
-              <TableHeadData key={field}>{field}</TableHeadData>
-            ))}
-          </TableHeadRow>
-        </TableHead>
-        <tbody>
-          {customers.map(({ name, company, phone, email, country, status }) => {
-            return (
-              <TableRow key={name}>
-                <TableData>{name}</TableData>
-                <TableData>{company}</TableData>
-                <TableData>{phone}</TableData>
-                <TableData>{email}</TableData>
-                <TableData>{country}</TableData>
-                <TableData>
-                  <Status isActive={status === 'Active'}>{status}</Status>
-                </TableData>
-              </TableRow>
-            );
-          })}
-        </tbody>
-      </StyledTable>
-    </>
+    <StyledTable>
+      <TableHead>
+        <TableHeadRow>
+          {fields.map(field => (
+            <TableHeadData key={field}>{field}</TableHeadData>
+          ))}
+        </TableHeadRow>
+      </TableHead>
+      <tbody>
+        {customers.map(({ name, company, phone, email, country, status }) => {
+          return (
+            <TableRow key={name}>
+              <TableData>{name}</TableData>
+              <TableData>{company}</TableData>
+              <TableData>{phone}</TableData>
+              <TableData>{email}</TableData>
+              <TableData>{country}</TableData>
+              <TableData>
+                <Status isActive={status === 'Active'}>{status}</Status>
+              </TableData>
+            </TableRow>
+          );
+        })}
+      </tbody>
+    </StyledTable>
   );
 }
